7326d9a742d9f7baad3ca62c0eb4cf8ecac01a0c
Arthur PAOLINI <arthur.paolini@mines-paristech.fr>
2022-04-27 18:20:37 +0200

    sound design

--- rogue-in-the-cloud-reseauto/static/js/main.js
+++ rogue-in-the-cloud-reseauto/static/js/main.js
@@ -44,24 +44,32 @@ window.addEventListener("DOMContentLoaded", (event) => {
     btn_n.onclick = function(e) {
         console.log("Clicked on button north");
         socket.emit("move", {dx:0, dy:-1});
+        var audio = document.getElementById("music");
+        audio.play();
     };
 
     var btn_s = document.getElementById("go_s");
     btn_s.onclick = function(e) {
         console.log("Clicked on button south");
         socket.emit("move", {dx:0, dy:1});
+        var audio = document.getElementById("music");
+        audio.play();
     };
 
     var btn_w = document.getElementById("go_w");
     btn_w.onclick = function(e) {
         console.log("Clicked on button w");
         socket.emit("move", {dx:-1,dy:0});
+        var audio = document.getElementById("music");
+        audio.play();
     };
 
     var btn_e = document.getElementById("go_e");
     btn_e.onclick = function(e) {
         console.log("Clicked on button e");
         socket.emit("move", {dx:1, dy:0});
+        var audio = document.getElementById("music");
+        audio.play();
     };
 
 
@@ -74,4 +82,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
     });
 
+    
+
 });
+
+
+
